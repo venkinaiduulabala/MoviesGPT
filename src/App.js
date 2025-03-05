bde2725b140cfef937e04ee0cf4375d8ae974eab
venkinaiduulabala <ulabalavenkinaidu>
2025-03-05 16:37:27 +0530

    tailwind

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="color-redtext-10xl font-bold underline text-center text-5xl text-red-100">
+      
+        
+        
+        <h1>Naidu</h1>
+        
+         
+      
+  
+    </div>
+  );
+}
+
+export default App;
